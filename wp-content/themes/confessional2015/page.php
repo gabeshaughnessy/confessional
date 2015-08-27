32c208b946a6a01daa5b74e8ac12592a0814dbaa
gabeshaughnessy <gabeshaughnessy@gmail.com>
2015-08-27 06:11:16 -0700

    submit form with enter key

--- wp-content/themes/confessional2015/page.php
+++ wp-content/themes/confessional2015/page.php
@@ -1,7 +1,18 @@
 <?php 
 global $post;
 get_header();
-
+?>
+<script>
+jQuery(document).ready(function(){
+	jQuery("form").keypress(function(event) {
+	    if (event.which == 13) {
+	        event.preventDefault();
+	        jQuery("#submit").click();
+	    }
+	});
+});
+</script>
+<?php
 if(have_posts()) : while(have_posts()) : the_post();
 
 echo '<article>';
